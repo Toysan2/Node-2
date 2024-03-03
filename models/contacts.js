25e0893e03abf79c17bdf495c837895cd82b0046
Toy Santana António <toysan3@hotmail.com>
2024-03-03 08:40:19 +0100

    Update

--- models/contacts.js
+++ models/contacts.js
@@ -1,70 +1,59 @@
-const Joi = require("joi");
-const fs = require("fs/promises");
-const path = require("path");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const contactsPath = path.join(__dirname, "contacts.json");
+const connectionString =
+  "mongodb+srv://toysan3:BvNf%253Rimongodb@cluster0.nw4xzpi.mongodb.net/";
 
-const contactSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().min(7).max(15).required(),
-});
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connection successful"))
+  .catch((err) => {
+    console.error("Database connection error", err);
+    process.exit(1);
+  });
 
-const readContacts = async () => {
-  const data = await fs.readFile(contactsPath, "utf8");
-  return JSON.parse(data);
-};
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-const writeContacts = async (contacts) => {
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-};
+const Contact = mongoose.model("Contact", contactSchema);
 
-const listContacts = async () => {
-  return await readContacts();
-};
-
-const getContactById = async (contactId) => {
-  const contacts = await readContacts();
-  return contacts.find((contact) => contact.id === contactId);
-};
-
-const removeContact = async (contactId) => {
-  const contacts = await readContacts();
-  const index = contacts.findIndex((contact) => contact.id === contactId);
-  if (index === -1) {
-    return { error: "Contact with this ID does not exist" };
-  }
-  const [removedContact] = contacts.splice(index, 1);
-  await writeContacts(contacts);
-  return removedContact;
-};
-
+const listContacts = async () => await Contact.find();
+const getContactById = async (contactId) => await Contact.findById(contactId);
+const removeContact = async (contactId) =>
+  await Contact.findByIdAndRemove(contactId);
 const addContact = async (body) => {
-  const { error } = contactSchema.validate(body);
-  if (error) {
-    throw new Error(error.details[0].message);
-  }
-
-  const contacts = await readContacts();
-  const newContact = { id: require("uuid").v4(), ...body };
-  contacts.push(newContact);
-  await writeContacts(contacts);
-  return newContact;
+  const contact = new Contact(body);
+  await contact.save();
+  return contact;
 };
-
-const updateContact = async (contactId, body) => {
-  const { error } = contactSchema.validate(body);
-  if (error) {
-    throw new Error(error.details[0].message);
-  }
-
-  const contacts = await readContacts();
-  const index = contacts.findIndex((contact) => contact.id === contactId);
-  if (index === -1) return null;
-  contacts[index] = { ...contacts[index], ...body };
-  await writeContacts(contacts);
-  return contacts[index];
-};
+const updateContact = async (contactId, body) =>
+  await Contact.findByIdAndUpdate(contactId, body, { new: true });
+const updateStatusContact = async (contactId, body) =>
+  await Contact.findByIdAndUpdate(
+    contactId,
+    { favorite: body.favorite },
+    { new: true }
+  );
 
 module.exports = {
   listContacts,
@@ -72,4 +61,5 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
+  updateStatusContact,
 };
