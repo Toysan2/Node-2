8ecf29d33d4fcd1b251efe7e67f70ad04e49e86d
Toy Santana António <toysan3@hotmail.com>
2024-03-04 17:36:40 +0100

    Update

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,21 +4,17 @@ const { register, login } = require("../../controllers/authController");
 const { authenticateToken } = require("../../middleware/authMiddleware");
 const User = require("../../models/userModel");
 
-// POST route for user registration
 router.post("/register", async (req, res) => {
   register(req, res);
 });
 
-// POST route for user login
 router.post("/login", async (req, res) => {
   login(req, res);
 });
 
-// GET route for fetching current user's profile
 router.get("/current", authenticateToken, async (req, res) => {
   try {
-    // Since authenticateToken middleware is used, req.user is already populated
-    const user = await User.findById(req.user._id).select("-password -token"); // Exclude sensitive information
+    const user = await User.findById(req.user._id).select("-password -token");
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
