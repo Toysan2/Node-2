3065c9a4e5efe91d6f78defd4acabc55c6caa221
Toy Santana António <toysan3@hotmail.com>
2024-03-05 15:40:07 +0100

    Update
    Done:
    Step 1 - created public folder that gives statistics
    Step 2 - Used Gravatar package
    Step 3 - save of avatarURL
    Step 4 - added PATCH method

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 const contactsRouter = require("./routes/api/contacts");
 const usersRouter = require("./routes/api/users");
+
+mongoose
+  .connect(process.env.DB_CONNECTION_STRING)
+  .then(() => console.log("MongoDB connection established"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const app = express();
 app.use(express.static("public"));
@@ -16,8 +22,8 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
-app.use("/api/users", usersRouter);
+app.use("/contacts", contactsRouter);
+app.use("/users", usersRouter);
 
 app.use((req, res, next) => {
   res.status(404).json({ message: "Not found" });
